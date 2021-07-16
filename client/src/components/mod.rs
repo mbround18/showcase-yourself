7fb9250215b416cbd00dfab9ca3d808fe63053c7
Michael <12646562+mbround18@users.noreply.github.com>
2021-07-16 15:18:50 -0700

    updated to work (#144)

--- client/src/components/mod.rs
+++ client/src/components/mod.rs
@@ -1,2 +1,4 @@
 pub mod footer;
+pub mod link;
 pub mod portrait;
+pub mod profile;
---
+++ client/src/components/profile.rs
@@ -0,0 +1,54 @@
+use crate::config::Profile;
+use yew::prelude::*;
+use yew::Html;
+
+pub fn render(profile: &Profile) -> Html {
+    let output = html! {
+        <div id="profile" class="p-4">
+            <label for="tagline" class="text-lg font-bold italic">{"Tagline"}</label>
+            <h1 id="tagline">{ &profile.tag_line }</h1>
+            <div id="summary">
+                { if ! profile.summary.is_empty() {
+                    html! {
+                        <p class="wrap">{ &profile.summary }</p>
+                    }
+                }  else {
+                    html! {}
+                } }
+            </div>
+            <div id="actively-looking">
+                {  if profile.actively_looking {
+                  html! {
+                    <>
+                        <label for="actively-looking" class="text-lg font-bold italic">{"Actively Looking!"}</label>
+                        <h3 id="actively-looking">{"Hey! I am actively looking for work!\
+                        If you think I fit what you are looking for, please reach out via LinkedIn!
+                        "}</h3>
+                        <label for="looking-for" class="text-lg font-bold italic">{"Looking For"}</label>
+                        <ul id="looking-for">
+                            { profile.looking_for.iter().map(|position| {
+                                html! {
+                                    <li>{ position }</li>
+                                }
+                            }).collect::<Html>() }
+                        </ul>
+                    </>
+                  }
+                } else {
+                  html! { }
+                } }
+            </div>
+            <div id="skills">
+                <label for="skill-list" class="text-lg font-bold italic">{"Skills"}</label>
+                <ul id="skill-list">{
+                    profile.skills.iter().map(|skill| {
+                        html! {
+                            <li>{ skill }</li>
+                        }
+                    }).collect::<Html>() }
+                </ul>
+            </div>
+        </div>
+    };
+    output
+}
